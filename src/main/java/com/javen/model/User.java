fbb702792e69fcdb5b10c47ddbfd3e059100d9ed
379753498 <379753498@qq.com>
2017-08-16 17:38:07 +0800

    wqe

--- src/main/java/com/javen/model/User.java
+++ src/main/java/com/javen/model/User.java
@@ -13,12 +13,14 @@ import org.springframework.stereotype.Component;
  * 
  *
  * CREATE TABLE `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `username` varchar(60) DEFAULT NULL COMMENT '用户名',
-  `password` varchar(60) DEFAULT NULL COMMENT '密码',
-  `state` int(11) DEFAULT NULL COMMENT '状态',
+  `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` VARCHAR(60) DEFAULT NULL COMMENT '用户名',
+   `Phone` VARCHAR(60) DEFAULT NULL COMMENT 'Phone',
+    `Email` VARCHAR(60) DEFAULT NULL COMMENT 'Email',
+  `EmailUUid` VARCHAR(60) DEFAULT NULL COMMENT 'EmailUUid',
+  `state` INT(11) DEFAULT NULL COMMENT '状态',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
+) ENGINE=INNODB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
 
  *@Title:  
  *@Description:  
@@ -28,11 +30,39 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class User implements Serializable {
+	public String getPhone() {
+		return Phone;
+	}
+
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+	public String getEmailUUid() {
+		return EmailUUid;
+	}
+
+	public void setEmailUUid(String emailUUid) {
+		EmailUUid = emailUUid;
+	}
+
+
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", state=" + state
-				+ ", password=" + password + "]";
+		return "User [id=" + id + ", username=" + username + ", state=" + state + ", password=" + password + ", Phone="
+				+ Phone + ", Email=" + Email + ", EmailUUid=" + EmailUUid + "]";
 	}
+
+
 
 	@Id
     @Column(name = "id")
@@ -41,6 +71,10 @@ public class User implements Serializable {
 	private String username;
 	private Integer state;
 	private String password;
+	private String Phone;
+	private String Email;
+	private String EmailUUid;
+	
 
 	public String getPassword() {
 		return password;
