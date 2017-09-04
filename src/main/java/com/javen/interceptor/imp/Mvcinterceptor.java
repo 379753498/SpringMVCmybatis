7be49fec7a63c72d1be8d4c287b5be39376732bf
379753498 <379753498@qq.com>
2017-09-04 16:12:30 +0800

    完善登录功能及其他相关js修改工作 安全权限拦截器修改

---
+++ src/main/java/com/javen/interceptor/imp/Mvcinterceptor.java
@@ -0,0 +1,66 @@
+package com.javen.interceptor.imp;
+
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.swing.text.rtf.RTFEditorKit;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.javen.interceptor.annotation.Sessionannotation;
+import com.javen.interceptor.annotation.Tokenannotation;
+import com.javen.model.User;
+
+public class Mvcinterceptor extends HandlerInterceptorAdapter {
+	private static final Logger LOG = Logger.getLogger(Tokenannotation.class);
+
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws Exception {
+		boolean isok= true;
+		if (handler instanceof HandlerMethod) {
+			
+			
+			HttpSession session = request.getSession();
+
+			HandlerMethod handlerMethod = (HandlerMethod) handler;
+			Method method = handlerMethod.getMethod();
+			Sessionannotation Sessionannotation = method.getAnnotation(Sessionannotation.class);
+		
+		 if(Sessionannotation!=null)
+		 {
+			 isok= lsok(request);
+		
+			 
+		 }
+
+		}
+		return isok;
+
+	}
+
+	private boolean lsok(HttpServletRequest request) {
+		try {
+			if (request.getServletPath() != ":/login") {
+				System.out.println(request.getServletPath()
+						+ "-----111111111111111");
+				User attribute = (User) request.getSession().getAttribute("user");
+				if (attribute == null) {
+					LOG.warn("NO  session User,url:" + request.getServletPath());
+
+					return false;
+				}
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return true;
+	}
+}
