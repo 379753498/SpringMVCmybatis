22b667d6818ab28535f6a43ffc891eba910ae00c
HelloWord <379753498@qq.com>
2018-07-18 13:50:38 +0800

    修改shell 脚本1

--- lib/jenkins.sh
+++ lib/jenkins.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-cd /home
+#dockerFile 文件所在路径 例如/home（必填）
+DockerFileDIR=
+#创建镜像的名称（必填）
+ImageName=
+#运行时容器名称
+RUNINGIMAGENAME=
+#映射的端口
+PORT=
+cd $DockerFileDIR
 #开始构建应用
-docker build -t tomcat:spring .
+docker build -t $ImageName .
 #批量删除 无用的镜像
 docker rmi $(docker images |awk '{if($2=="<none>")  print $3}')
 #删除程序包
@@ -9,11 +17,12 @@ rm -rf /home/SpringMVCmybatis.war
 #删除Dockerfile
 rm -rf /home/Dockerfile
 #强杀容器进程
-docker kill spring
+docker kill $RUNINGIMAGENAME
 #移除停止的容器
-docker rm spring
+docker rm $RUNINGIMAGENAME
 #查看目前在运行的容器
 docker ps
 #启动新镜像的容器
-docker run -d --name spring -p 8080:8080 tomcat:spring
-#vim filename set ff=unix wq
+docker run -d --name $RUNINGIMAGENAME  -p $PORT:$PORT $ImageName
+
+
